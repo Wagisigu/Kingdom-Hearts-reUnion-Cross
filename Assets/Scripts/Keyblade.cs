dad04b1daa9adf98c03e02310137a19f094e26be
Cameron Wagner <55332052+Wagisigu@users.noreply.github.com>
2022-02-19 22:55:28 -0600

    Basic fighting mech added

--- Assets/Scripts/Keyblade.cs
+++ Assets/Scripts/Keyblade.cs
@@ -1,28 +1,55 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Keyblade : MonoBehaviour
 {
-    public bool turn;
+    public GameObject player;
+    public GameObject target;
+    public float medal1;
+    public float medal2;
+    public float medal3;
+    public float medal4;
+    public float medal5;
+    public float enemeyHealth;
 
     private bool spin;
     private float lastZAngle;
     private float currentZAngle;
+    private int medalNum;
+    private float[] medalStrength;
+    private bool slain;
     // Start is called before the first frame update
     void Start()
     {
         spin = false;
+        slain = false;
         lastZAngle = 45f;
-        currentZAngle = 45f; 
-        turn = 1 == SceneManager.GetActiveScene().buildIndex;
+        currentZAngle = 45f;
+        medalNum = 0;
+        medalStrength = new float[] {
+            medal1,
+            medal2,
+            medal3,
+            medal4,
+            medal5
+        };
+        Debug.Log($"Enemy has {enemeyHealth} remaining");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (turn && Input.GetMouseButtonUp(0) && !spin)
+        if (Input.GetMouseButtonUp(0) && !spin)
         {
             spin = true;
+            enemeyHealth -= medalStrength[medalNum];
+            if (enemeyHealth > 0) Debug.Log($"Enemy has {enemeyHealth} remaining");
+            else
+            {
+                Debug.Log("Enemy slain");
+                slain = true;
+            }
+            medalNum = (medalNum + 1) % 5;
+            player.transform.Translate(new Vector3(3f, 0f, 0f));
         }
         if (spin)
         {
@@ -35,6 +62,11 @@ public class Keyblade : MonoBehaviour
                 transform.Rotate(0f, 0f, (Mathf.Abs(lastZAngle - currentZAngle) - 72));
                 currentZAngle %= 360;
                 lastZAngle = currentZAngle;
+                player.transform.Translate(new Vector3(-3f, 0f, 0f));
+                if (slain)
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+                }
             }
         }
     }
