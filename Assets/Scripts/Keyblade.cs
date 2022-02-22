38d02db31f1803c662c7bca85368d2ca1541bc6c
Cameron Wagner <55332052+Wagisigu@users.noreply.github.com>
2022-02-22 02:00:33 -0600

    added damage text

--- Assets/Scripts/Keyblade.cs
+++ Assets/Scripts/Keyblade.cs
@@ -4,19 +4,16 @@ public class Keyblade : MonoBehaviour
 {
     public GameObject player;
     public GameObject target;
-    public float medal1;
-    public float medal2;
-    public float medal3;
-    public float medal4;
-    public float medal5;
     public float enemeyHealth;
+    public float[] medals;
+    public TMPro.TextMeshProUGUI healthText;
 
     private bool spin;
     private float lastZAngle;
     private float currentZAngle;
     private int medalNum;
-    private float[] medalStrength;
     private bool slain;
+    private float a;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,13 +22,7 @@ public class Keyblade : MonoBehaviour
         lastZAngle = 45f;
         currentZAngle = 45f;
         medalNum = 0;
-        medalStrength = new float[] {
-            medal1,
-            medal2,
-            medal3,
-            medal4,
-            medal5
-        };
+        a = 255f;
         Debug.Log($"Enemy has {enemeyHealth} remaining");
     }
 
@@ -41,12 +32,17 @@ public class Keyblade : MonoBehaviour
         if (Input.GetMouseButtonUp(0) && !spin)
         {
             spin = true;
-            enemeyHealth -= medalStrength[medalNum];
+            a = 255f;
+            enemeyHealth -= medals[medalNum];
+            healthText.color = new Color(255f, 0f, 0f, 255f);
+            healthText.transform.localPosition = new Vector3(230f, 30f, 0f);
+            healthText.text = $"-{medals[medalNum]}";
             if (enemeyHealth > 0) Debug.Log($"Enemy has {enemeyHealth} remaining");
             else
             {
                 Debug.Log("Enemy slain");
                 slain = true;
+                healthText.color = new Color(0f, 0f, 0f, 0f);
             }
             medalNum = (medalNum + 1) % 5;
             player.transform.Translate(new Vector3(3f, 0f, 0f));
@@ -68,6 +64,12 @@ public class Keyblade : MonoBehaviour
                     UnityEngine.SceneManagement.SceneManager.LoadScene(0);
                 }
             }
+        }
+        if (a > 0f)
+        {
+            healthText.transform.Translate(0f, 25f * Time.deltaTime, 0f);
+            healthText.color = Color.Lerp(healthText.color, new Color(255f, 0f, 0f, 0f), 5f * Time.deltaTime);
+            a = healthText.color.a;
         }
     }
 }
